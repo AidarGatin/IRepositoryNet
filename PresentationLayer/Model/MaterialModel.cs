b21d930d244cfb3c383419e3423753c6c710b469
Dev DkHardware <dev@dkhardware.com>
2019-03-22 17:45:42 -0400

    Presentation layer added

---
+++ PresentationLayer/Model/MaterialModel.cs
@@ -0,0 +1,20 @@
+﻿using DataLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PresentationLayer.Model
+{
+	public class MaterialViewModel : PageViewModel
+	{
+		public Material Material { get; set; }
+		public Material NextMaterial { get; set; }
+
+	}
+	public class MaterialEditModel : PageEditModel
+	{
+		[Required]
+		public int Directoryid { get; set; }
+	}
+}
